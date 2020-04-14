3f1bc60852307591378916d750faf7decfef79e7
tomoko matsuo <matsuo09212001@yahoo.co.jp>
2020-04-14 11:47:43 +0900

    rails g で自動生成を防ぐ、及び、.env実装完了

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,10 @@ module INSTAGRAMExam
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
   end
 end
