03c8cf3d334c94e2ed9e903aa5e5d63becca463f
tomoko matsuo <matsuo09212001@yahoo.co.jp>
2020-04-16 18:34:20 +0900

    rooting変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'tops#index'
+  root 'users#new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show, :edit, :update]
   resources :blogs do
