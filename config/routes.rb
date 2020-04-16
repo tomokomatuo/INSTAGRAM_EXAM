ac04833f2098b4e61f4f5cc1743a8f0a594037bc
tomoko matsuo <matsuo09212001@yahoo.co.jp>
2020-04-16 18:15:58 +0900

    heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'blogs/index'
+  root 'tops#index'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show, :edit, :update]
   resources :blogs do
